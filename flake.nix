66a58e12cc09a7157d815af412ea52a96fca74f4
Elijah Massey <emassey0135@gmail.com>
2025-02-14 16:15:25 -0600

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let pkgs = import nixpkgs {
+        inherit system;
+      };
+        geyser = (with pkgs;
+          stdenv.mkDerivation {
+            pname = "geyser";
+            version = "1.0";
+            src = fetchurl {
+              url = "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/standalone";
+              hash = "sha256-WdSXDkH+3JudYzX/GMoHJaB1zM9Zy7gx7YwzdNTyuPU=";
+            };
+            phases = ["installPhase"];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src $out/bin/Geyser-Standalone.jar
+            '';
+          }
+        );
+      in rec {
+        defaultApp = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+        };
+        defaultPackage = geyser;
+      }
+    );
+}
